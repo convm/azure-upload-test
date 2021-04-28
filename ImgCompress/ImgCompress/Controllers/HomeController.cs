ff9ad8edaa7efed6dad3d537e79001d85bd32f9c
Wilson Fung <moidar@hotmail.com>
2021-04-28 12:01:25 +0800

    no message

---
+++ ImgCompress/ImgCompress/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ImgCompress.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ImgCompress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment hostingEnvironment)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+            var html = "";
+            var file = new FileInfo(webRootPath + "/file/123.jpg");
+            using (var d = file.OpenRead())
+            {
+                using (var ms = new MemoryStream())
+                {
+                    html += "<br/>Start Resize:"+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+                    ImageHelper.resizeImage("123.jpg", d, ms, new System.Drawing.Size(3000, 3000));
+                    html += "<br/>3000 Resize Complete:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+                    ImageHelper.resizeImage("123.jpg", d, ms, new System.Drawing.Size(2000, 2000));
+                    html += "<br/>2000 Resize Complete:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+                    ImageHelper.resizeImage("123.jpg", d, ms, new System.Drawing.Size(400, 400));
+                    html += "<br/>400 Resize Complete:" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+                }
+            };
+
+            return View(new HtmlDTO { html = html });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class HtmlDTO { 
+    
+        public string html { set; get; }
+    }
+}
---
+++ ImgCompress/ImgCompress/ImgHelper.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ImgCompress
+{
+    public static class ImageHelper
+    {
+        /// <summary>
+        /// 是否为图片
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsImg(string name)
+        {
+
+            var ilist = new List<string> {
+            ".jpg",".png",".jpeg",".gif",
+            };
+            return ilist.Where(i => name.ToLower().Contains(i)).Count() > 0 ? true : false;
+
+        }
+
+
+        ///
+        /// 根据指定尺寸得到按比例缩放的尺寸,返回true表示以更改尺寸
+        ///
+        /// 图片宽度
+        /// 图片高度
+        /// 指定宽度
+        /// /// 指定高度
+        /// 返回true表示以更改尺寸
+        public static bool GetPicZoomSize(ref int picWidth, ref int picHeight, int specifiedWidth, int specifiedHeight)
+        {
+            int sW = 0, sH = 0;
+            Boolean isZoomSize = false;
+            //按比例缩放
+            System.Drawing.Size tem_size = new System.Drawing.Size(picWidth, picHeight);
+            if (tem_size.Width > specifiedWidth || tem_size.Height > specifiedHeight) //将**改成c#中的或者操作符号
+            {
+                if ((tem_size.Width * specifiedHeight) > (tem_size.Height * specifiedWidth))
+                {
+                    sW = specifiedWidth;
+                    sH = (specifiedWidth * tem_size.Height) / tem_size.Width;
+                }
+                else
+                {
+                    sH = specifiedHeight;
+                    sW = (tem_size.Width * specifiedHeight) / tem_size.Height;
+                }
+                isZoomSize = true;
+            }
+            else
+            {
+                sW = tem_size.Width;
+                sH = tem_size.Height;
+            }
+            picHeight = sH;
+            picWidth = sW;
+            return isZoomSize;
+        }
+        /// <summary>
+        ///  无损压缩图片
+        /// </summary>
+        /// <param name="sFile"></param>
+        /// <param name="dFile"></param>
+        /// <param name="HWratio">高宽比例压缩</param>
+        /// <param name="flag">质量压缩1 -100</param>
+        /// <returns></returns>
+
+
+
+        #region Azure blob 等比压缩
+             
+        private static ImageFormat GetEncoder(string extension)
+        {
+            ImageFormat encoder = null;
+
+            extension = extension.Replace(".", "");
+
+            var isSupported = Regex.IsMatch(extension, "gif|png|jpe?g", RegexOptions.IgnoreCase);
+
+            if (isSupported)
+            {
+                switch (extension.ToLower())
+                {
+                    case "png":
+                        encoder = ImageFormat.Png;
+                        break;
+                    case "jpg":
+                        encoder = ImageFormat.Jpeg;
+                        break;
+                    case "jpeg":
+                        encoder = ImageFormat.Jpeg;
+                        break;
+                    case "gif":
+                        encoder = ImageFormat.Gif;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return encoder;
+        }
+
+        public static void resizeImage(string Url, Stream sFile, MemoryStream output, Size size)
+        {
+            using (System.Drawing.Image imgToResize = System.Drawing.Image.FromStream(sFile))
+            {
+                //Get the image current width  
+                int sourceWidth = imgToResize.Width;
+                //Get the image current height  
+                int sourceHeight = imgToResize.Height;
+                float nPercent = 0;
+                float nPercentW = 0;
+                float nPercentH = 0;
+                //Calulate  width with new desired size  
+                nPercentW = ((float)size.Width / (float)sourceWidth);
+                //Calculate height with new desired size  
+                nPercentH = ((float)size.Height / (float)sourceHeight);
+                if (nPercentH < nPercentW)
+                    nPercent = nPercentH;
+                else
+                    nPercent = nPercentW;
+                //New Width  
+                int destWidth = (int)(sourceWidth * nPercent);
+                //New Height  
+                int destHeight = (int)(sourceHeight * nPercent);
+                var extension = Path.GetExtension(Url);
+                using (Bitmap b = new Bitmap(destWidth, destHeight))
+                {
+                    Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    // Draw image with new width and height  
+                    g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+                    g.Dispose();
+                    b.Save(output, GetEncoder(extension));
+                }
+
+            }
+        }
+
+
+        #endregion
+
+
+        public static void Resize(string Url, Stream sFile, MemoryStream output, int width, int height)
+        {
+            using (System.Drawing.Image image = System.Drawing.Image.FromStream(sFile))
+            {
+                var destRect = new Rectangle(0, 0, width, height);
+                using (var destImage = new Bitmap(width, height))
+                {
+
+                    destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+                    using (var graphics = Graphics.FromImage(destImage))
+                    {
+                        graphics.CompositingMode = CompositingMode.SourceCopy;
+                        graphics.CompositingQuality = CompositingQuality.HighQuality;
+                        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        graphics.SmoothingMode = SmoothingMode.HighQuality;
+                        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                        using (var wrapMode = new ImageAttributes())
+                        {
+                            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                        }
+                    }
+                }
+                var extension = Path.GetExtension(Url);
+                image.Save(output, GetEncoder(extension));
+            }
+        }
+
+    }
+}
